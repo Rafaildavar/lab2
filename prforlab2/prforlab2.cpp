8c6d104ed6280369c48c5535b820a322daddd132
Rafaildavar <rafail.davar@rambler.ru>
2022-11-06 14:08:03 +0300

    Сделан вариант 4

---
+++ prforlab2/prforlab2.cpp
@@ -0,0 +1,12 @@
+﻿int main()
+{
+	long double q = 1.2 + 0.85 - 0.01;
+	long double w = q / 0.2;
+	long double e = 61 + (1 / 3) + (20 / 3);
+	long double r = w / e;
+	long double t = 13.5 + 11;
+	long double y = 52 * 7 / 52;
+	long double u = t / y;
+	long double i = r + u - 0.1;
+	return 0;
+}
